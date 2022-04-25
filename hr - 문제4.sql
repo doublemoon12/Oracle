33498d98b833c27da4ee56c4ba3a869766daed43
Moonjoon Kim <doublemoon@kakao.com>
2022-04-25 18:32:20 +0900

    2022-04-25

--- hr - 문제4.sql
+++ hr - 문제4.sql
@@ -1,2 +1,43 @@
--- 문제4
+-- 학원 문제
+select ename 사원이름, salary 월급
+from employee
+where salary not BETWEEN 1000 and 1500;
+
+select ename 사원이름, hiredate 입사일
+from employee
+where hiredate like '81%';
+
+select *
+from employee
+where (substr(hiredate, 1, 2)) = 87;
+
+select round(MONTHS_BETWEEN(SYSDATE,hiredate)) 개월수
+from employee;
+
+select dno, sum(salary)
+from employee
+group by dno
+having SUM(salary) > 3000;
+
+select dno 부서번호, count(dno) 부서별사원수, round(avg(salary), 2)평균급여
+from employee
+GROUP by dno;
+
+select e.ename, e.dno, d.dname
+from employee e, department d
+where e.dno = d.dno and e.ename = 'SCOTT';
+
+SELECT ename, dname, loc
+FROM employee e NATURAL join department d
+where commission is not null and commission != 0;
+
+select ename, salary, dno
+from employee
+where salary in (select min(salary) from employee group by dno)
+
+
+
+
+
+
 
--- hr~문제3sql.sql
+++ hr~문제3sql.sql
@@ -44,8 +44,6 @@ select e.eno, e.ename, m.ename, m.eno
 from employee e, employee m
 where e.manager = m.eno;
 
-select *
-from employee;
 
 -- 8.
 select *
@@ -61,11 +59,7 @@ where (e.dno = m.dno) and e.ename = 'SCOTT';
 -- 10.
 select e.ename 이름, e.hiredate
 from employee e, employee m
-where (e.hiredate = m.hiredate) and e.hiredate(e.ename = 'WARD') > e.hiredate;
-
-select e.ename 이름, e.hiredate, m.hiredate
-from employee e, employee m
-where e.hiredate = m.hiredate;
+where m.ename = 'WARD' and e.hiredate > m.hiredate;
 
 select ename, hiredate
 from employee
@@ -87,8 +81,116 @@ from employee
 where salary > (select salary from employee where eno = 7499);
 
 -- 3.
-select job, ename
-from employee 
+select job, ename, salary
+from employee
+where salary in(select min(salary) from employee group by job);
+
+-- 4.
+select job, min(salary)
+from employee
 group by job
-having job = (select job from employee)
+having avg(salary) = (select min(avg(salary)) from employee group by job);
+
+select job, min(salary)
+from employee
+group by job
+having avg(salary) <= all(select avg(salary) from employee group by job);
+
+select ename, job, salary
+from employee
+where salary = (select min(salary) from employee GROUP BY job having avg(salary) = (select min(avg(salary)) from employee group by job));
+
+-- 5.
+select ename, salary, dno
+from employee
+where salary in (select min(salary) from employee group by dno);
+
+-- 6.
+select eno, ename, job, salary
+from employee
+where salary < any(select salary from employee where job = 'ANALYST') and job <> 'ANALYST';
+
+-- 7.
+select ename
+from employee
+where eno not in (select distinct manager from employee where manager is not null);
+
+-- 8.
+select ename
+from employee
+where eno in (select distinct manager from employee);
+
+-- 9.
+select ename, hiredate
+from employee
+where dno = (select dno from employee where ename = 'BLAKE') and ename <> 'BLAKE';
+
+-- 10.
+select eno, ename, salary
+from employee
+where salary > (select avg(salary) from employee)
+order by salary;
+
+-- 11.
+select eno, ename
+from employee
+where dno in (select dno from employee where ename like '%K%');
+
+-- 12.
+select ename, dno, job
+from employee 
+where dno = (select dno from department where loc = 'DALLAS');
+
+select e.ename, e.dno, e.job, d.loc
+from employee e, department d
+where e.dno = d.dno
+and d.loc = 'DALLAS';
+
+-- 13.
+select ename, salary
+from employee
+where manager = (select eno from employee where ename = 'KING');
+
+-- 14.
+select dno, ename, job
+from employee
+where dno = (select dno from department where dname = 'RESEARCH');
+
+select e.ename, eno, e.job, d.dname
+from employee e, department d
+where e.dno = d.dno
+and d.dname = 'RESEARCH';
+
+-- 15.
+select eno, ename, salary
+from employee
+where (salary > (select avg(salary) from employee)) and dno in (select dno from employee where ename like '%M%');
+
+select eno, ename, salary
+from employee
+where dno in (select dno from employee where (ename like '%M%') and (salary > (select avg(salary) from employee)));
+
+-- 16.
+select job, avg(salary)
+from employee
+group by job
+having avg(salary) = (select min(avg(salary)) from employee group by job);
+
+-- 17. 
+select *
+from employee
+where dno in (select dno from employee where job = 'MANAGER');
+
+select *
+from employee
+order by dno;
+
+
+
+
+
+
+
+
+
 
